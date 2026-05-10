12a408c69daa48ad3f66b9a9c69b615a7b534fd4
diego <caipediego541@gmail.com>
2026-05-10 09:50:18 -0400

    resolver ocnflictos y corregir app

--- estacionamiento_app.py
+++ estacionamiento_app.py
@@ -1,31 +1,41 @@
+from flask import Flask
 from dotenv import load_dotenv
 import os
-from flask import Flask
+
 from config.db import db
-from models import Vehiculo
+
+from models.vehiculo import Vehiculo
+from models.ingreso import Ingreso
+from models.egreso import Egreso
+
 from routes.vehiculo_r import vehiculo_bp
 from routes.ingreso_r import ingreso_bp
-from models import registrar
+from routes.egreso_r import egreso_bp
 
 load_dotenv()
+
 app = Flask(__name__)
 
-
+# Configuración de base de datos
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD") or ""
 host = os.getenv("DB_HOST")
 dbname = os.getenv("DB_NAME")
 
-uri = f"mysql+pymysql://{user}:{password}@{host}/{dbname}"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{user}:{password}@{host}/{dbname}"
+)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app) 
-#rutas:
+# Inicializar DB
+db.init_app(app)
+
+# Registrar rutas
 app.register_blueprint(vehiculo_bp)
 app.register_blueprint(ingreso_bp)
-app.register_blueprint(registrar_bp)
+app.register_blueprint(egreso_bp)
+
 
 if __name__ == "__main__":
     with app.app_context():
