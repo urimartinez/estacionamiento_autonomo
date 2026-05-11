6c88ff16f9a8a837cb13b39749a9fc9354b21702
Urimartinez <martinezuri20@gmail.com>
2026-05-10 23:51:10 -0300

    estacionamiento_app modificado en cliente espacio y tarifa con sus rutas y registros blueprint

--- estacionamiento_app.py
+++ estacionamiento_app.py
@@ -3,6 +3,14 @@ from config.db import db
 from dotenv import load_dotenv
 import os
 
+from models.cliente import Cliente
+from models.espacio import Espacio
+from models.tarifa import Tarifa
+
+from routes.ruta_cliente import cliente_bp
+from routes.ruta_espacio import espacio_bp
+from routes.ruta_tarifa import tarifa_bp
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -10,6 +18,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 
 db.init_app(app)
+
+app.register_blueprint(cliente_bp)
+app.register_blueprint(espacio_bp)
+app.register_blueprint(tarifa_bp)
 
 if __name__ == "__main__":
     with app.app_context():
